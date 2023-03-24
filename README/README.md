dd8e37176c154eb657fc496999f4d722fd14e2b9
jonny desktop <jon@uslms.com>
2023-03-23 18:30:07 -0600

    Added comments and changed the file structure, also added a README file

---
+++ README/README.md
@@ -0,0 +1,9 @@
+## 05 Third-Party APIs: Work Day Scheduler
+# Description
+The Work Day Scheduler is a simple calendar application that allows users to save events for each hour of the day. The application features dynamically updated HTML and CSS powered by jQuery and uses the Day.js library to work with date and time.
+
+When the application is opened, the current day is displayed at the top of the calendar. Scrolling down reveals time blocks for standard business hours, with each time block color-coded to indicate whether it is in the past, present, or future. Users can click into a time block and enter an event. Clicking the save button for that time block saves the text for that event in local storage.
+
+Even after refreshing the page, the saved events persist thanks to local storage. With this application, busy employees can easily manage their time effectively by adding important events to a daily planner.
+
+<img src="samplePic.jpg">
--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,16 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// const AM9 = $('hour-9')
-// const AM10 = $('hour-10')
-// const AM11 = $('hour-11')
-// const PM12 = $('hour-12')
-// const PM1 = $('hour-13')
-// const PM2 = $('hour-14')
-// const PM3 = $('hour-15')
-// const PM4 = $('hour-16')
-// const PM5 = $('hour-17')
-
 const timeCard = $('.timeCard');
-console.log(timeCard)
-
-
-
 const currentDay = $('#currentDay')
-currentDay.text(dayjs().format('H'))
+
+currentDay.text(dayjs().format('M-DD-YYYY'))
 
 let currentHour = 0;
 let pastHour = 0;
-
 function timeCardColor(){
-  //currentHour = (dayjs().format('H'))
-  currentHour = 15
+  currentHour = (dayjs().format('H'))
   //This checks if there has been a change in the hour and if there was it changes the color of the cards
   if(currentHour !== pastHour){
     for(let i=0; i<timeCard.length; i++){
+      // I subtract 9 to sink up the index number with the hour
       if(i < currentHour - 9)
         timeCard[i].className = "row time-block past timeCard"
       if(i === currentHour - 9){
@@ -41,11 +23,11 @@ function timeCardColor(){
     pastHour=currentHour;
   }
 }
-
 timeCardColor()
 //Checks every minute
 setInterval(timeCardColor, 60000)
 
+//Pulls the saved values from local storage 
 function init(){
   for(let i=0; i<timeCard.length; i++){
     if(localStorage.getItem(timeCard[i].id) !== null){
@@ -54,19 +36,15 @@ function init(){
   }
 }
 
-
+//Creates a click listener on all of the save buttons
 function saveCard() { 
   for(let i=0; i<timeCard.length; i++){
-    
-    // addEventListener NOT jQuery
-    timeCard[i].children[2].addEventListener("click", function(){
-      // .value NOT jQuery
-      console.log(timeCard[i].children[1].value + timeCard[i].id)
-      localStorage.setItem(timeCard[i].id, timeCard[i].children[1].value)
-
+    $(timeCard[i].children[2]).on("click", function(){
+      //Creates a unique item in local storage with the id
+      localStorage.setItem(timeCard[i].id, $(timeCard[i].children[1]).val())
     })
-
   }
 };
+
 init()
 saveCard()
